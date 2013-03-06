8bba547a89856d9fc8cd499136120eb0cf11f179
name <no@no.com>
2013-03-05 22:11:10 -0500

    working breakpoint

--- setup_dev_mode.sh
+++ setup_dev_mode.sh
@@ -2,4 +2,4 @@
 
 #FINALIZED DEV CODE (depswriter.py)
 #http://code.google.com/closure/library/docs/depswriter.html
-./closure-library/closure/bin/build/depswriter.py --output_file=closure-myfiles/deps.js --root_with_prefix="js ../../../../js"
+./closure-library/closure/bin/build/depswriter.py --output_file=closure-myfiles/deps.js --root_with_prefix="js ../../../js" --root_with_prefix="unit-tests ../../../unit-tests"
